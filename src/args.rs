80209e497ad04ffb64f6f4ac0a92269150274569
thepotatocoder <dbviana733@gmail.com>
2017-11-03 20:42:20 +0000

    Added pattern.rs. Moved args struct and methods into args.rs.

---
+++ src/args.rs
@@ -0,0 +1,105 @@
+
+extern crate clap;
+
+use std::fmt;
+use std::str::FromStr;
+use std::string::String;
+
+pub struct Args {
+    pub input_file: String,
+    pub file_pattern: Vec<String>,
+    pub suffix_format: String,
+    pub prefix: String,
+    pub keep_files: bool,
+    pub suppress_matched: bool,
+    pub digits: i32,
+    pub quiet: bool,
+    pub elide_empty_files: bool,
+    //help: bool,
+    //version: bool,
+}
+
+impl fmt::Display for Args {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f,
+		"\tInput: '{:?}'\n\tFile Pattern: {:?}\n\tSuffix Format: {}\n\tPrefix: {}\n\tKeep files: {}\n\t\
+		Suppress matched: {}\n\tDigits: {}\n\tQuiet: {}\n\tElide empty files: {}\n",
+			self.input_file, self.file_pattern, self.suffix_format, self.prefix, self.keep_files,
+			self.suppress_matched, self.digits, self.quiet, self.elide_empty_files)
+	}
+}
+
+impl Args {
+	//TODO: Handle errors better
+	pub fn from_matches(matches: &clap::ArgMatches) -> Args {
+		let mut args = Args {
+ 	       input_file: String::default(),
+ 	       file_pattern: Vec::new(),
+ 	       suffix_format: String::from("%02d"),
+ 	       prefix: String::from("xx"),
+ 	       keep_files: false,
+ 	       suppress_matched: false,
+ 	       digits: 2,
+ 	       quiet: false,
+ 	       elide_empty_files: false,
+ 	   };
+
+    	args.input_file = match matches.value_of("INPUT") {
+			Some(a) => String::from(a),
+			None => String::from("this will never happen right?"),
+		};
+
+		//TODO: Error Handling?
+    	args.file_pattern = matches.values_of("FILE_PATTERN").unwrap().map(String::from).collect();
+
+		args.suffix_format = match matches.value_of("SUFFIX_FORMAT") {
+	        Some(a) => a.to_string(),
+	        None => {
+				if matches.occurrences_of("SUFFIX_FORMAT") == 1 {
+					String::from("this will never happen right?")
+				} else {
+					String::from("%02d")
+				}
+			}   
+		};
+	
+		args.prefix = match matches.value_of("PREFIX") {
+	        Some(a) => a.to_string(),
+			None => {
+				if matches.occurrences_of("PREFIX") == 1 {
+					String::from("this will never happen right?")
+				} else {
+					String::from("xx")
+				}
+			}
+
+	    };
+
+		if matches.occurrences_of("KEEP_FILES") != 0 {
+			args.keep_files = true;
+		}
+	
+		if matches.occurrences_of("SUPPRESS_MATCHED") != 0 {
+			args.suppress_matched = true;
+		}
+
+	    args.digits = match FromStr::from_str(
+	        match matches.value_of("DIGITS"){
+	            Some(s) => s,
+	            None => "2",
+	        }) {
+	            Ok(a) => a,
+	            Err(_) => panic!("Invalid number"),
+	        };
+	
+		if matches.occurrences_of("QUIET") != 0 {
+			args.quiet = true;
+		}
+
+		if matches.occurrences_of("ELIDE_EMPTY_FILES") != 0 {
+			args.elide_empty_files = true;
+		}
+
+	    args
+	}
+}
---
+++ src/pattern.rs
@@ -0,0 +1,79 @@
+
+extern crate grep;
+
+use args;
+
+use std::fmt;
+use std::str::FromStr;
+
+//Pattern matching
+//		INTEGER				copy up to but not including specified line number
+//		/REGEXP/[OFFSET]	copy up to but not including a matching line
+//		%REGEXP%[OFFSET]	skip to, but not including a matching line
+//		{INTEGER}			repeat the previous pattern specified number of times
+//		{*}					repeat the previous pattern as many times as possible
+//A line OFFSET is a required '+' or '-' followed by a positive integer.
+
+pub struct Pattern {
+	pub id: usize,
+	pub repeats_left: i32,
+	pub regex: grep::Grep,
+	pub repeater: bool,
+}
+
+impl fmt::Display for Pattern {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f,
+		"Pattern:\n\tID: {}\n\tRepeats left: {}\n\tRegex: {:?}\n\tRepeater: {}\n",
+		self.id, self.repeats_left, self.regex, self.repeater)
+	}
+}
+
+pub fn from_args(args: &args::Args) -> (Vec<Pattern>, String) {
+	let mut patterns: Vec<Pattern> = Vec::new();
+		
+		for (i, patt) in args.file_pattern.iter().enumerate() {
+		if patt.starts_with("{") {
+			//How does this work?
+			if (i == 0) || (patterns[i-1].repeater) {
+				return (patterns, format!("csplit: '{}': invalid pattern\n", patt));
+			}
+
+			let chars_to_trim: &[char] = &['{', '}'];
+			let s = patt.trim_matches(chars_to_trim);
+
+			if s == "*" {
+				patterns[i-1].repeats_left = -1;
+			}
+
+			if s.chars().all(|c| c.is_numeric()) {
+				patterns[i-1].repeats_left = FromStr::from_str(s).unwrap();
+			}
+
+			let regex = patterns[i-1].regex.clone();
+
+			patterns.push(Pattern {
+				id: i,
+				repeats_left: 0,
+				//Regex value doesn't matter.
+				//Maybe find a better way?
+				regex,
+				repeater: true
+			});
+		} else {
+			let regex = match grep::GrepBuilder::new(patt).build() {
+				Ok(r) => r,
+				Err(_) => panic!("Could not compile regex"),
+			};
+
+			patterns.push(Pattern {
+				id: i,
+				repeats_left: 1,
+				regex,
+				repeater: false
+			});
+		}
+	}
+
+	(patterns, "".to_string())
+}
